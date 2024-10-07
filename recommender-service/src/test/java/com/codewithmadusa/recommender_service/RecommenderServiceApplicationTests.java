59eb3239b2d8a7874dc3d4d55120eb85ca818f24
blaise <blaise@kali>
2024-10-07 16:17:08 +0100

    setting up  our microservices structure

---
+++ recommender-service/src/test/java/com/codewithmadusa/recommender_service/RecommenderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithmadusa.recommender_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommenderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-service/src/test/java/com/codewithmadusa/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithmadusa.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
